bafb9a2f5fb23ed5ce86acc6641767ca0f2dd36f
James Waddington <jmw.code@googlemail.com>
2022-07-12 09:08:20 +0100

    Break the Record feature in to a component
    By decoupling the component from the route we can embed it on different pages.

--- src/Record.js
+++ src/Record.js
@@ -1,58 +1,13 @@
-import { useState } from "react";
 import { useParams } from "react-router-dom";
-import { saveFeeling } from "./mock-api";
-import styled from "styled-components";
 import useProjectDetails from "./hooks/useProjectDetails";
-import EditableTagList from "./EditableTagList";
-import FeelingButton from "./FeelingButton";
-
-const FeelingsButtonWrapper = styled.div`
-    width: 100%;
-`;
-
-const FeelingsForm = styled.form`
-    border-radius: 10px;
-    border-style: solid;
-    border-width: 2px;
-    padding: 1rem;
-    text-align: center;
-    width: 20rem;
-`;
-
-const SaveButton = styled.input`
-    background-color: green;
-    border: none;
-    color: #FFFFFF;
-    margin: 1rem 0;
-    padding: .5rem 0;
-    width: 100%;
-`;
+import RecordFeeling from "./RecordFeeling";
 
 const Record = () => {
     const {project} = useParams();
-    const [selectedValue, setSelectedValue] = useState(0);
-    const [tags, setTags] = useState([]);
-    const [projectDetails] = useProjectDetails(project)
-
-    const handleSubmit = async event => {
-        event.preventDefault();
-        await saveFeeling({
-            value: selectedValue,
-            tags
-        });
-    };
+    const [projectDetails] = useProjectDetails(project);
 
     return (
-        <FeelingsForm onSubmit={handleSubmit}>
-            <h2>{projectDetails.title}</h2>
-            <FeelingsButtonWrapper>
-                {projectDetails && projectDetails.icons.map((icon, index) => (
-                    <FeelingButton key={index} icon={icon} isSelected={index === selectedValue} clickCallback={() => setSelectedValue(index)} />
-                ))}
-            </FeelingsButtonWrapper>
-            <EditableTagList tags={tags} setTags={setTags} />
-            <SaveButton type="submit" value="save" />
-        </FeelingsForm>
+        <RecordFeeling projectDetails={projectDetails} />
     );
 };
 
---
+++ src/RecordFeeling.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { saveFeeling } from "./mock-api";
+import styled from "styled-components";
+import EditableTagList from "./EditableTagList";
+import FeelingButton from "./FeelingButton";
+
+const FeelingsButtonWrapper = styled.div`
+    width: 100%;
+`;
+
+const FeelingsForm = styled.form`
+    border-radius: 10px;
+    border-style: solid;
+    border-width: 2px;
+    padding: 1rem;
+    text-align: center;
+    width: 20rem;
+`;
+
+const SaveButton = styled.input`
+    background-color: green;
+    border: none;
+    color: #FFFFFF;
+    margin: 1rem 0;
+    padding: .5rem 0;
+    width: 100%;
+`;
+
+const RecordFeeling = ({projectDetails}) => {
+    const [selectedValue, setSelectedValue] = useState(0);
+    const [tags, setTags] = useState([]);
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        await saveFeeling({
+            value: selectedValue,
+            tags
+        });
+    };
+
+    return (
+        <FeelingsForm onSubmit={handleSubmit}>
+            <h2>{projectDetails.title}</h2>
+            <FeelingsButtonWrapper>
+                {projectDetails && projectDetails.icons.map((icon, index) => (
+                    <FeelingButton key={index} icon={icon} isSelected={index === selectedValue} clickCallback={() => setSelectedValue(index)} />
+                ))}
+            </FeelingsButtonWrapper>
+            <EditableTagList tags={tags} setTags={setTags} />
+            <SaveButton type="submit" value="save" />
+        </FeelingsForm>
+    );
+};
+
+export default RecordFeeling;
